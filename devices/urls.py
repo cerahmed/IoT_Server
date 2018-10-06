468d9a729884cd216c01238590d9a621116f640b
Ahmed Alyami <cer.ahmed@gmail.com>
2018-10-06 12:32:52 +0300

    changed all urlpattern instances of re_path to path

--- devices/urls.py
+++ devices/urls.py
@@ -1,10 +1,10 @@
-from django.urls import re_path
+from django.urls import path
 from . import views
 
 app_name = 'devices'
 
 urlpatterns = [
-    re_path(r'^toggle/$', views.toggle, name='toggle'),
-    re_path(r'^turnon/$', views.turnOn, name='turnon'),
-    re_path(r'^turnoff/$', views.turnOff, name='turnoff')
+    path('toggle/', views.toggle, name='toggle'),
+    path('turnon/', views.turnOn, name='turnon'),
+    path('turnoff/', views.turnOff, name='turnoff')
     ]
